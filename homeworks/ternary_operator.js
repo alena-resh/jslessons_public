081bf69953642f7e8dbdcc42374c1f4b04de9445
alena-resh <reshetnikovaalenna@gmail.com>
2022-10-17 15:31:18 +0300

    Revert "ternary operator"
    This reverts commit d7012c19a685984331ba52f58a28f80a619d0627.

--- homeworks/ternary_operator.js
+++ homeworks/ternary_operator.js
@@ -1,5 +1,4 @@
 // Ternary Operator
-
 // Complete function to describe the age, <= 12 - kid, >= 13 && age <= 17 - teenager, age >= 18 && age <= 64 - elderly
 
 // function describeAge(age) {
@@ -32,20 +31,19 @@
   # +---------------+-------------+
   # |n>=10          |     90      |
   # +---------------+-------------+
+*/
 
-
-    if (n < 5){
-      return n * 100
-    }
-    else if ( n >= 5 && n< 100)
-      {
-        return n * 95
-      }
-    else{
-    return n * 90
-      }
-  }
-  */
+  //   if (n < 5){
+  //     return n * 100
+  //   }
+  //   else if ( n >= 5 && n< 100)
+  //     {
+  //       return n * 95
+  //     }
+  //   else{
+  //   return n * 90
+  //     }
+  // }
 
   function saleHotdogs(n){
     return n<5 ? n*100 : n>=5 && n<10 ? n*95 : n*90;
