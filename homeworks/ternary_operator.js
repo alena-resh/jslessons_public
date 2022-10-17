5e9f69bb176c98751e289a32f7b295d8b22ee3e4
alena-resh <reshetnikovaalenna@gmail.com>
2022-10-17 15:27:32 +0300

    ternary operator

--- homeworks/ternary_operator.js
+++ homeworks/ternary_operator.js
@@ -1,4 +1,5 @@
 // Ternary Operator
+
 // Complete function to describe the age, <= 12 - kid, >= 13 && age <= 17 - teenager, age >= 18 && age <= 64 - elderly
 
 // function describeAge(age) {
@@ -31,19 +32,20 @@
   # +---------------+-------------+
   # |n>=10          |     90      |
   # +---------------+-------------+
-*/
 
-  //   if (n < 5){
-  //     return n * 100
-  //   }
-  //   else if ( n >= 5 && n< 100)
-  //     {
-  //       return n * 95
-  //     }
-  //   else{
-  //   return n * 90
-  //     }
-  // }
+
+    if (n < 5){
+      return n * 100
+    }
+    else if ( n >= 5 && n< 100)
+      {
+        return n * 95
+      }
+    else{
+    return n * 90
+      }
+  }
+  */
 
   function saleHotdogs(n){
     return n<5 ? n*100 : n>=5 && n<10 ? n*95 : n*90;
