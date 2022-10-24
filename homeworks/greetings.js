99f96719ad7da198e8109bf4a15adf7c6755ed8d
alena-resh <reshetnikovaalenna@gmail.com>
2022-10-24 15:24:58 +0300

    adding greetings function

---
+++ homeworks/greetings.js
@@ -0,0 +1,7 @@
+// Write a function that creates and outputs to the console a custom greeting with the name that you pass to the function.
+
+function greetCustomer(name) {
+    let greetings = `Good evening ${name}, \nI am glad to see you! Welcome to our restaurant!\n`
+    console.log(greetings)
+}
+greetCustomer("Arkadiy")
